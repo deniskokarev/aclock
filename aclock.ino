100bd1d424a2acfbae17d6667b6b4388aca5616c
Denis Kokarev <d_kokarev@mail.ru>
2016-11-19 23:06:41 -0500

    adding sleep/wakeup in rtc timer, optional temperature display in C or F

--- aclock.ino
+++ aclock.ino
@@ -1,3 +1,4 @@
+#include "Arduino.h"
 #include "RTClib.h"
 #include "Time.h"
 #include "Timezone.h"
@@ -5,8 +6,14 @@
 #include "Adafruit_PCD8544.h"
 #include <avr/sleep.h>
 
+const byte tempOptPin = 2; // F/C selection
+enum TEMP_OPT {
+	TEMP_OPT_C = 0,
+	TEMP_OPT_F = 1
+};
 RTC_DS3231 rtc;
-const int LMT86Pin = A3;		// analog temperature sensor
+const byte rtcInterruptPin = 3; // rtc can wake us up every second
+const byte LMT86Pin = A3;		// analog temperature sensor
 const float aVRefmV = 3270.0;	// analog reference voltage in mV
 //US Eastern Time Zone (New York, Detroit)
 TimeChangeRule myDST = {"EDT", Second, Sun, Mar, 2, -240};	  //Daylight time = UTC - 4 hours
@@ -15,9 +22,9 @@ Timezone myTZ(myDST, mySTD);
 // Hardware SPI (faster, but must use certain hardware pins):
 // SCK is LCD serial clock (SCLK) - this is pin 13 on Arduino Uno
 // MOSI is LCD DIN - this is pin 11 on an Arduino Uno
-const int scePin = 6;   // SCE - Chip select
-const int rstPin = 7;   // RST - Reset
-const int dcPin = 5;    // DC - Data/Command
+const byte scePin = 6;   // SCE - Chip select
+const byte rstPin = 7;   // RST - Reset
+const byte dcPin = 5;    // DC - Data/Command
 Adafruit_PCD8544 display = Adafruit_PCD8544(dcPin, scePin, rstPin);
 
 static void displayWarnSetTime() {
@@ -36,8 +43,10 @@ void setup() {
 	// init done
 	// you can change the contrast around to adapt the display
 	// for the best viewing!
-	display.setContrast(50);
+	display.setContrast(40);
 
+	pinMode(rtcInterruptPin, INPUT_PULLUP);
+	pinMode(tempOptPin, INPUT);
 	Serial.begin(9600);
 	while (!Serial) {
 		; // wait for serial port to connect. Needed for native USB port only
@@ -80,10 +89,12 @@ void setup() {
 
 		display.clearDisplay();
 	}
+	// enabling 1Hz timer from rtc (connected to rtcInterruptPin)
+	rtc.writeSqwPinMode(DS3231_SquareWave1Hz);
 }
 
 //Function to print time with time zone
-void printTemp(time_t t, char *tz, int temperature) {
+void printTemp(time_t t, char *tz, float temperature) {
 	char buf[16];
 	display.fillScreen(WHITE),
 	display.setTextSize(1);
@@ -97,7 +108,13 @@ void printTemp(time_t t, char *tz, int temperature) {
 	Serial.print(buf);
 	Serial.print(' ');
 	display.println(buf);
-	snprintf(buf, sizeof(buf), "TEMP: %02d C", temperature);
+	switch(digitalRead(tempOptPin)) {
+	case TEMP_OPT_C:
+		snprintf(buf, sizeof(buf), "TEMP: %02d C", int(temperature));
+		break;
+	default: // TEMP_OPT_F
+		snprintf(buf, sizeof(buf), "TEMP: %02d F", int(temperature*(9.0/5.0)+32.0));
+	}
 	Serial.print(buf);
 	Serial.print(' ');
 	display.println(buf);
@@ -113,12 +130,35 @@ float getTempLMT86(short pin) {
 	return t;
 }
 
-void loop() {
+static void refresh() {
 	TimeChangeRule *tcr;		//pointer to the time change rule, use to get TZ abbrev
 	time_t local;
 	time_t utc = rtc.now().unixtime();
 	local = myTZ.toLocal(utc, &tcr);
 	printTemp(local, tcr->abbrev, getTempLMT86(LMT86Pin));
-	delay(1000);
 }
 
+static void wake () {
+	// nop
+}
+
+
+void sleepNow()			// here we put the arduino to sleep
+{
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here
+ 
+	sleep_enable();			 // enables the sleep bit in the mcucr register
+							 // so sleep is possible. just a safety pin
+	attachInterrupt(digitalPinToInterrupt(rtcInterruptPin), wake, FALLING);
+ 
+	sleep_mode();			 // here the device is actually put to sleep!!
+							 // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
+	sleep_disable();		 // first thing after waking from sleep:
+							 // disable sleep...
+	detachInterrupt(digitalPinToInterrupt(rtcInterruptPin));
+}
+
+void loop() {
+	sleepNow();
+	refresh();
+}
